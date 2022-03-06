62038e988fca24002ac9a2fca99c7588c46514f5
Arlindo Da Silva Kohls <62305577+Kohls20@users.noreply.github.com>
2022-03-06 11:54:05 -0300

    Alteração TESTE 1

--- app/src/main/java/com/example/aula1/MainActivity.kt
+++ app/src/main/java/com/example/aula1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.aula1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//TESTE 1
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
